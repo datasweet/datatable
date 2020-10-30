d699d34807c2008eaee796f689a0f039d309f221
Romain Vibrac <romain.vibrac@datasweet.fr>
2020-10-30 14:46:57 +0100

    Fix tests

--- join_test.go
+++ join_test.go
@@ -81,12 +81,12 @@ func TestLeftJoin(t *testing.T) {
 	assert.NotNil(t, dt)
 
 	checkTable(t, dt,
-		"id", "prenom", "nom", "email", "ville", "concat", "date_achat", "num_facture", "prix_total",
-		1, "Aimée", "Marechal", "aime.marechal@example.com", "Paris", "AiméeMarechal", time.Date(2013, time.January, 23, 0, 0, 0, 0, time.UTC), "A00103", 203.14,
-		1, "Aimée", "Marechal", "aime.marechal@example.com", "Paris", "AiméeMarechal", time.Date(2013, time.February, 14, 0, 0, 0, 0, time.UTC), "A00104", 124.00,
-		2, "Esmée", "Lefort", "esmee.lefort@example.com", "Lyon", "EsméeLefort", time.Date(2013, time.February, 17, 0, 0, 0, 0, time.UTC), "A00105", 149.45,
-		3, "Marine", "Prevost", "m.prevost@example.com", "Lille", "MarinePrevost", time.Date(2013, time.February, 21, 0, 0, 0, 0, time.UTC), "A00106", 235.35,
-		4, "Luc", "Rolland", "lucrolland@example.com", "Marseille", "LucRolland", nil, nil, nil,
+		"id", "prenom", "nom", "email", "ville", "date_achat", "num_facture", "prix_total",
+		1, "Aimée", "Marechal", "aime.marechal@example.com", "Paris", time.Date(2013, time.January, 23, 0, 0, 0, 0, time.UTC), "A00103", 203.14,
+		1, "Aimée", "Marechal", "aime.marechal@example.com", "Paris", time.Date(2013, time.February, 14, 0, 0, 0, 0, time.UTC), "A00104", 124.00,
+		2, "Esmée", "Lefort", "esmee.lefort@example.com", "Lyon", time.Date(2013, time.February, 17, 0, 0, 0, 0, time.UTC), "A00105", 149.45,
+		3, "Marine", "Prevost", "m.prevost@example.com", "Lille", time.Date(2013, time.February, 21, 0, 0, 0, 0, time.UTC), "A00106", 235.35,
+		4, "Luc", "Rolland", "lucrolland@example.com", "Marseille", nil, nil, nil,
 	)
 }
 
@@ -98,12 +98,12 @@ func TestRightJoin(t *testing.T) {
 	assert.NotNil(t, dt)
 
 	checkTable(t, dt,
-		"id", "prenom", "nom", "email", "ville", "concat", "date_achat", "num_facture", "prix_total",
-		1, "Aimée", "Marechal", "aime.marechal@example.com", "Paris", "AiméeMarechal", time.Date(2013, time.January, 23, 0, 0, 0, 0, time.UTC), "A00103", 203.14,
-		1, "Aimée", "Marechal", "aime.marechal@example.com", "Paris", "AiméeMarechal", time.Date(2013, time.February, 14, 0, 0, 0, 0, time.UTC), "A00104", 124.00,
-		2, "Esmée", "Lefort", "esmee.lefort@example.com", "Lyon", "EsméeLefort", time.Date(2013, time.February, 17, 0, 0, 0, 0, time.UTC), "A00105", 149.45,
-		3, "Marine", "Prevost", "m.prevost@example.com", "Lille", "MarinePrevost", time.Date(2013, time.February, 21, 0, 0, 0, 0, time.UTC), "A00106", 235.35,
-		nil, nil, nil, nil, nil, "", time.Date(2013, time.March, 2, 0, 0, 0, 0, time.UTC), "A00107", 47.58,
+		"id", "prenom", "nom", "email", "ville", "date_achat", "num_facture", "prix_total",
+		1, "Aimée", "Marechal", "aime.marechal@example.com", "Paris", time.Date(2013, time.January, 23, 0, 0, 0, 0, time.UTC), "A00103", 203.14,
+		1, "Aimée", "Marechal", "aime.marechal@example.com", "Paris", time.Date(2013, time.February, 14, 0, 0, 0, 0, time.UTC), "A00104", 124.00,
+		2, "Esmée", "Lefort", "esmee.lefort@example.com", "Lyon", time.Date(2013, time.February, 17, 0, 0, 0, 0, time.UTC), "A00105", 149.45,
+		3, "Marine", "Prevost", "m.prevost@example.com", "Lille", time.Date(2013, time.February, 21, 0, 0, 0, 0, time.UTC), "A00106", 235.35,
+		nil, nil, nil, nil, nil, time.Date(2013, time.March, 2, 0, 0, 0, 0, time.UTC), "A00107", 47.58,
 	)
 }
 
@@ -115,29 +115,24 @@ func TestOuterJoin(t *testing.T) {
 	assert.NotNil(t, dt)
 
 	checkTable(t, dt,
-		"id", "prenom", "nom", "email", "ville", "concat", "date_achat", "num_facture", "prix_total",
-		1, "Aimée", "Marechal", "aime.marechal@example.com", "Paris", "AiméeMarechal", time.Date(2013, time.January, 23, 0, 0, 0, 0, time.UTC), "A00103", 203.14,
-		1, "Aimée", "Marechal", "aime.marechal@example.com", "Paris", "AiméeMarechal", time.Date(2013, time.February, 14, 0, 0, 0, 0, time.UTC), "A00104", 124.00,
-		2, "Esmée", "Lefort", "esmee.lefort@example.com", "Lyon", "EsméeLefort", time.Date(2013, time.February, 17, 0, 0, 0, 0, time.UTC), "A00105", 149.45,
-		3, "Marine", "Prevost", "m.prevost@example.com", "Lille", "MarinePrevost", time.Date(2013, time.February, 21, 0, 0, 0, 0, time.UTC), "A00106", 235.35,
-		4, "Luc", "Rolland", "lucrolland@example.com", "Marseille", "LucRolland", nil, nil, nil,
+		"id", "prenom", "nom", "email", "ville", "date_achat", "num_facture", "prix_total",
+		1, "Aimée", "Marechal", "aime.marechal@example.com", "Paris", time.Date(2013, time.January, 23, 0, 0, 0, 0, time.UTC), "A00103", 203.14,
+		1, "Aimée", "Marechal", "aime.marechal@example.com", "Paris", time.Date(2013, time.February, 14, 0, 0, 0, 0, time.UTC), "A00104", 124.00,
+		2, "Esmée", "Lefort", "esmee.lefort@example.com", "Lyon", time.Date(2013, time.February, 17, 0, 0, 0, 0, time.UTC), "A00105", 149.45,
+		3, "Marine", "Prevost", "m.prevost@example.com", "Lille", time.Date(2013, time.February, 21, 0, 0, 0, 0, time.UTC), "A00106", 235.35,
+		4, "Luc", "Rolland", "lucrolland@example.com", "Marseille", nil, nil, nil,
 		nil, nil, nil, nil, nil, time.Date(2013, time.March, 2, 0, 0, 0, 0, time.UTC), "A00107", 47.58,
 	)
 }
 
 func TestInnerJoinWithExprOnHidden(t *testing.T) {
 	customers, orders := sampleForJoin()
-	// customers.AddColumn("upper_ville", datatable.String, datatable.Expr("UPPER(ville)"))
 	customers.AddColumn("id2", datatable.Int, datatable.Expr("`id`+100"))
 	orders.AddColumn("user_id2", datatable.Int, datatable.Expr("`user_id`+100"))
 	customers.HideColumn("id")
 	dt, err := customers.InnerJoin(orders, datatable.On("[Customers].[id2]", "[Orders].[user_id2]"))
 	assert.NoError(t, err)
 	assert.NotNil(t, dt)
-	// col := dt.Column("upper_ville")
-	// assert.Equal(t, datatable.String, col.Type())
-	// assert.Equal(t, "NullString", col.UnderlyingType().Name())
-	// assert.True(t, col.IsComputed())
 
 	fmt.Println(dt)
 
@@ -152,16 +147,11 @@ func TestInnerJoinWithExprOnHidden(t *testing.T) {
 
 func TestLeftJoinWithExpr(t *testing.T) {
 	customers, orders := sampleForJoin()
-	// customers.AddColumn("upper_ville", datatable.String, datatable.Expr("UPPER(ville)"))
 	customers.AddColumn("id2", datatable.Int, datatable.Expr("`id`+100"))
-	orders.AddColumn("user_id2", datatable.Int, datatable.Expr("`user_id`+100-1"))
+	orders.AddColumn("user_id2", datatable.Int, datatable.Expr("`user_id`+100"))
 	dt, err := customers.LeftJoin(orders, datatable.On("[Customers].[id2]", "[Orders].[user_id2]"))
 	assert.NoError(t, err)
 	assert.NotNil(t, dt)
-	// col := dt.Column("upper_ville")
-	// assert.Equal(t, datatable.String, col.Type())
-	// assert.Equal(t, "NullString", col.UnderlyingType().Name())
-	// assert.True(t, col.IsComputed())
 
 	fmt.Println(dt)
 
@@ -177,16 +167,11 @@ func TestLeftJoinWithExpr(t *testing.T) {
 
 func TestRightJoinWithExpr(t *testing.T) {
 	customers, orders := sampleForJoin()
-	// customers.AddColumn("upper_ville", datatable.String, datatable.Expr("UPPER(ville)"))
 	customers.AddColumn("id2", datatable.Int, datatable.Expr("`id`+100"))
 	orders.AddColumn("user_id2", datatable.Int, datatable.Expr("`user_id`+100"))
 	dt, err := customers.RightJoin(orders, datatable.On("[Customers].[id2]", "[Orders].[user_id2]"))
 	assert.NoError(t, err)
 	assert.NotNil(t, dt)
-	// col := dt.Column("upper_ville")
-	// assert.Equal(t, datatable.String, col.Type())
-	// assert.Equal(t, "NullString", col.UnderlyingType().Name())
-	// assert.True(t, col.IsComputed())
 
 	fmt.Println(dt)
 
